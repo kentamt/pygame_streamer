9f07a0553f013292b58dde44dc216ab280e8cd01
kentam <kentabell0725@gmail.com>
2022-08-05 12:55:20 +0100

    update

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import subprocess as sp
 
 
 import numpy as np
-# import cv2  # opencv-python
+import cv2  # opencv-python
 import pygame
 import ffmpeg  # ffmpeg-python, https://github.com/kkroening/ffmpeg-python
 
@@ -24,8 +24,8 @@ def main():
 
     # Set up the drawing window
     w, h = 320, 240
-    x, y = 50, 50
-    r = 20
+    x, y = 0, 120
+    r = 16
     screen = pygame.display.set_mode([w, h])
 
     # init ffmpeg process
@@ -33,7 +33,7 @@ def main():
     # video_format = 'mp4'  # you cannot use mp4 for pipe
     # video_format = 'flv'
     # video_format = 'ts'
-    video_format = 'mpegts'
+    # video_format = 'mpegts'
 
     # url = 'http://localhost:8080'
     rtmp_url = 'rtmp://localhost:1935/stream'
@@ -43,20 +43,37 @@ def main():
     # writing_process = (
     #     ffmpeg
     #     .input('pipe:', format='rawvideo', codec="rawvideo", pix_fmt='bgr24', s='{}x{}'.format(w, h))
+    #     .setpts('2.6*PTS')  #
     #     .output(
-    #         http_url,
+    #         'udp://239.0.0.1:1234?ttl=13',
     #         pix_fmt="yuv420p",
     #         vcodec="libx264",  # use same codecs of the original video
     #         listen=1,  # enables HTTP server
     #         movflags='frag_keyframe+empty_moov',
-    #         # movflags='frag_keyframe',
-    #         # movflags='empty_moov',
     #         preset="veryfast",
     #         max_muxing_queue_size=512,
-    #         f=video_format)
+    #         f='mp4')
     #     # .global_args("-re")  # argument to act as a live stream
     #     .run_async(pipe_stdin=True)
     # )
+
+    # writing_process = (
+    #     ffmpeg
+    #     .input('pipe:', format='rawvideo', codec="rawvideo", pix_fmt='bgr24', s='{}x{}'.format(w, h))
+    #     # .setpts('2.6*PTS')  #
+    #     .output(
+    #         'http://localhost:8080',
+    #         # 'udp://239.0.0.1:1234?ttl=13',
+    #         pix_fmt="yuv420p",
+    #         vcodec="libx264",  # use same codecs of the original video
+    #         listen=1,  # enables HTTP server
+    #         movflags='frag_keyframe+empty_moov',
+    #         preset="veryfast",
+    #         max_muxing_queue_size=512,
+    #         f='flv')
+    #     .run_async(pipe_stdin=True)
+    # )
+
 
     # writing_process = (
     #     ffmpeg
@@ -70,22 +87,90 @@ def main():
     #     .run_async(pipe_stdin=True)
     # )
 
-    writing_process = (
-        ffmpeg
-        .input('pipe:', format='rawvideo', codec="rawvideo", pix_fmt='bgr24', s='{}x{}'.format(w, h))
-        .output(
-            "rtp://239.0.0.1:50004",
-            pix_fmt="yuv420p",
-            vcodec="libx264",
+    # writing_process = (
+    #     ffmpeg
+    #     .input('pipe:', format='rawvideo', codec="rawvideo", pix_fmt='bgr24', s='{}x{}'.format(w, h))
+    #     .output(
+    #         "rtp://239.0.0.1:50004",
+    #         pix_fmt="yuv420p",
+    #         vcodec="libx264",
+    #         preset="veryfast",
+    #         sdp_file='saved_sdp_file',
+    #         f='rtp')
+    #     .run_async(pipe_stdin=True)
+    # )
 
-            preset="veryfast",
-            sdp_file='saved_sdp_file',
-            f='rtp')
-        .run_async(pipe_stdin=True)
-    )
+    # HLS
+    # writing_process = (
+    #     ffmpeg
+    #     .input('pipe:', format='rawvideo', codec="rawvideo", pix_fmt='bgr24', s='{}x{}'.format(w, h))
+    #     .setpts('2.6*PTS')  # TODO: Magic number ...
+    #     .output(
+    #         '/var/www/html/hls/live.m3u8',
+    #         pix_fmt="yuv420p",
+    #         vcodec="libx264",
+    #         preset="ultrafast",
+    #         hls_time="1",
+    #         hls_flags="delete_segments",
+    #         segment_wrap="10",
+    #         f='hls')
+    #     .run_async(pipe_stdin=True)
+    # )
 
-    # ffplay_process = sp.Popen(['ffplay', url])
+    # DASH https://www.codeinsideout.com/blog/pi/stream-ffmpeg-hls-dash/#use-mpeg-dash-streaming
+    # mkdir -p /dev/shm/dash
+    # ln -s /dev/shm/dash /var/www/html/dash
+    # writing_process = (
+    #     ffmpeg
+    #     .input('pipe:', format='rawvideo', codec="rawvideo", pix_fmt='bgr24', s='{}x{}'.format(w, h))
+    #     .setpts('2.6*PTS')  # TODO: Magic number for 10FPS ...
+    #     .output(
+    #         '/dev/shm/dash/live.mpd',
+    #         # v="debug",
+    #         pix_fmt="yuv420p",
+    #         vcodec="libx264",
+    #         preset="ultrafast",
+    #         video_bitrate=1000000,
+    #         seg_duration=2,
+    #         streaming=1,
+    #         window_size=10,
+    #         remove_at_exit=1,
+    #         f='dash')
+    #     # .global_args("-r 10")  # argument to act as a live stream
+    #     .run_async(pipe_stdin=True)
+    # )
+    #
 
+    dimension = '{}x{}'.format(w, h)
+    f_format = 'bgr24'  # remember OpenCV uses bgr format
+    command = ['ffmpeg',
+               '-y',
+               '-f', 'rawvideo',
+               '-vcodec', 'rawvideo',
+               '-pix_fmt', 'bgr24',
+               '-s', dimension,
+               # '-r', '10',
+               '-re',
+               '-i', '-',
+               # '-an',
+               '-c:v', 'libx264',  #
+               '-pix_fmt', "yuv420p",
+               '-b:v', '2000k',
+               '-preset', 'ultrafast',
+               '-seg_duration', '5',
+               # '-streaming', '1',
+               '-window_size', '10',
+               '-remove_at_exit', '1',
+               '-r', '10',
+               '-f', 'dash',
+               '/dev/shm/dash/live.mpd']
+
+
+
+    writing_process = sp.Popen(command, stdin=sp.PIPE)
+
+    font = pygame.font.Font(pygame.font.get_default_font(), 15)
+    counter = 0
     running = True
     while running:
 
@@ -103,19 +188,19 @@ def main():
             pygame.draw.circle(screen, (50, 255, 100), (__x, __y), __s)
 
         pygame.draw.circle(screen, (0, 0, 255), (x, y), r)
-        pygame.display.flip()
 
-        # move circle
-        x += 10
-        if x > w + r:
-            x = -r
+        text_src = font.render(f'{counter}', False, (255,255,255))
+        screen.blit(text_src, (10, 10))
+
+        pygame.display.flip()
 
         # numpy and opencv part-------------------------------------------------
 
         array_data: np.array = pygame.surfarray.array3d(screen)
         array_data = array_data.astype(np.uint8)
         array_data = array_data.swapaxes(0, 1)
-        # array_data_bgr = cv2.cvtColor(array_data, cv2.COLOR_RGB2BGR)
+        array_data = cv2.cvtColor(array_data, cv2.COLOR_RGB2BGR)
+        # array_data[:,:,0], array_data[:,:,2] = array_data[:,:,2], array_data[:,:,0]
 
         # show array data using cv2
         # cv2.imshow('screen', array_data_bgr)
@@ -126,14 +211,18 @@ def main():
         # input array_data as byte data via stdin
         writing_process.stdin.write(array_data.tobytes())
         # writing_process.stdin.write(array_data_bgr.tobytes())
-        pygame.time.wait(100)
+        # pygame.time.wait(1)
+        time.sleep(0.1)
+
+        # move circle
+        x += 32
+        if x > w + r:
+            x = 0
+        counter += 1
 
     pygame.quit()
     writing_process.stdin.close()
-    writing_process.wait()
-
-    # writing_process.kill()
-    ffplay_process.kill()
+    writing_process.kill()
 
 
 if __name__ == '__main__':
